23031c08c25b0fdd7c5d9f05a9cbf641767a5119
LloganWolf <47220075+LloganWolf@users.noreply.github.com>
2019-11-12 15:10:51 +0100

    Correction DB
    Vidage du contenu de la base de données initiale

--- backend/db/leitmotiv.sql
+++ backend/db/leitmotiv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 08 nov. 2019 à 00:47
+-- Généré le :  mar. 12 nov. 2019 à 14:09
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -38,16 +38,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `modified_at` datetime DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `messages`
---
-
-INSERT INTO `messages` (`id`, `title`, `content`, `category`, `created_at`, `modified_at`, `user_id`) VALUES
-(1, 'Premier message', 'Ceci est le premier message que j\'ecris depuis cette nouvelle API', 'Positivité', '2019-11-08 01:03:25', NULL, 1),
-(2, 'Deuxieme message', 'Ceci est le deuxieme message que j\'ecris depuis cette nouvelle API. J\'ai modifié des champs et d\'autres, non', 'Croyance', '2019-11-08 01:04:45', '2019-11-08 01:09:21', 1),
-(3, 'Troisieme message', 'Ceci est le troisieme message que j\'ecris depuis cette nouvelle API. Il vient d\'un autre utilisateur et modifié', 'Croyance', '2019-11-08 01:12:58', '2019-11-08 01:20:42', 2);
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -64,15 +55,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_at` datetime NOT NULL,
   `connected_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `login`, `email`, `password`, `created_at`, `connected_at`) VALUES
-(1, 'LWolf', 'llogan.wolf@hotmail.fr', '$2b$05$uPqcf6/SKzD5629CVi6RXekCEKrAlOiNAoo7veQDtZlQH0O0l.QT.', '2019-11-08 00:55:59', '2019-11-08 00:58:16'),
-(2, 'JLucien', 'jeremie.lucien@hotmail.fr', '$2b$05$o1fXuxSK9quJBZXNBzFFmujDTgHc/47KOmr5w9bEgYyKgEp3TFiqu', '2019-11-08 01:10:45', '2019-11-08 01:10:52');
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
